db4fe30f0af44e160aed47824cb322d17b9381d2
Anas Abid <abid.anas3@gmail.com>
2021-11-03 14:48:00 -0700

    chp 14-20

---
+++ app.js
@@ -0,0 +1,161 @@
+// alert("HELLO CLASS")
+
+// 1)
+// var arr = [] ;
+
+// 2)
+// var arr = [] ;
+// console.log(arr)
+
+// 3)  
+//  var strings =  ["Muhammad","Anas","Abid"]
+
+
+// 4) 
+//  var numbers =  [205,457,874]
+
+
+// 5)  
+//  var boolean =  [10 < 8]
+
+
+// 6)  
+//  var mixArray =  ["Muhammad",7,7<8]
+
+
+//7)
+// var qualifications = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M.Phil.", "PhD"]
+// a=1
+// for (a=0;a<qualifications.length; ++a){
+    
+//     document.write(`${a}${qualifications[a]}`)
+//     document.write("<br>")
+//     // document.write(2+2)
+// }
+
+
+//8)
+// var names = ["Anas","Ali","Ahmed"]
+// var scores = [400,350,480]
+
+// // console.log(names.length)
+// // console.log(names)
+// // console.log(names[0])
+// // console.log(names[1])
+// // console.log(names[2])
+// // console.log(400*100/500)
+
+// for(i=0;i<names.length;i++){
+//     document.write(`score of ${names[i]} ${scores[i]}.Percentage: ${scores[i]*100/500}%`)
+//     document.write("<br>")
+    
+
+// }
+
+
+
+//9)
+// var colorss = ["red","blue","white","black"]
+// console.log(colorss)
+// document.write(colorss +"<br>")
+// var addClBeg = prompt("Add Color at beginning ")
+// colorss.unshift(addClBeg)
+// document.write(colorss)
+// var addClEnd = prompt("Add color at end")
+// colorss.push(addClEnd)
+// document.write("<br>"+colorss+"<br>")
+// colorss.unshift("color1","color2")
+// document.write(colorss)
+// colorss.shift(colorss[0])
+// document.write("<br>"+colorss)
+// colorss.pop(colorss)
+// document.write("<br>"+colorss)
+// var addClAtIndex = prompt("add at index num") 
+// var addClAtIndexCl = prompt("addat index color name")
+// colorss.splice( addClAtIndex,0,addClAtIndexCl)
+// document.write("<br>"+colorss)
+// console.log(colorss)
+// var clDelIndex = prompt("Delete from index:")
+// var qtyDel = prompt("Quantity Delete")
+// colorss.splice(clDelIndex,qtyDel)
+// document.write(colorss)
+// function sortArray(){
+
+
+//10)
+    // var sortNum = [320,230,480,120]
+    // sortNum.sort()
+    // // console.log(sortNum)
+    // document.write("Scores of students :"+sortNum)
+    // document.write(" <br>Ordered scores of Students:"+sortNum)
+// }
+// sortArray();
+
+
+
+//11)
+// var cityList = ["karachi","islamabad","quetta","Peshawar"]
+// var selectedCities = ["karachi","lahore"]
+
+
+
+//12)
+var arr =["This","is","My","cat"]
+arr.join(String)
+document.write(arr)
+
+//15)
+//var mobileNames = ("Apple","Samsung"," Motorola", "Nokia", "Sony" , "Haier")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
